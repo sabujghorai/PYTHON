a851cc51f738df0df14e598cde3bfcb0efe953d4
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-28 20:50:00 +0530

    adding the don't at line no 10

--- break_continue.py
+++ break_continue.py
@@ -7,7 +7,7 @@ while i<=5:
      break
   i += 1
 
-  print("loop terminate") # this print is inside the loop because tab is given at the first
+  print("loop doesn't terminate") # this print is inside the loop because tab is given at the first
 print("loop turminate") # and this print is outside the loop
 
 # CONTINUE STATEMENT
