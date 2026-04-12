9506bbca1abb1dd296f7f79536275e437b1d9f00
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-12 21:09:21 +0530

    updated the pythonproblem file

--- Basic_Python/pythonproblem.py
+++ Basic_Python/pythonproblem.py
@@ -1,4 +1,1 @@
-print("hello world")
-# hey hello how are you users i am writing this line for fun this line does not have any meaning 
-print("hello world")
-''' hey user i am using the triple cotes for multi line comment '''
+# Write a program that checks if a number is even or odd.
