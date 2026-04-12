e3b480bae792a02cc815a7d2cb7f555eb965284a
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-12 21:23:12 +0530

    updated the python problem file

--- Basic_Python/pythonproblem.py
+++ Basic_Python/pythonproblem.py
@@ -1,8 +1,12 @@
-# Write a program that checks if a number is even or odd.
-a = int(input("Enter a nimber :"))
-if a%2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
+# # Write a program that checks if a number is even or odd.
+# a = int(input("Enter a nimber :"))
+# if a%2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
 
 # Take a list of numbers and return their sum
+number = [1,3,4,2,5,7,9]
+
+total = sum(number)
+print("sum is = ",total)
