d0cf0cb699a4424c8a4325e529346a9cd3564c0d
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-30 14:04:59 +0530

    updated the typecasing file

--- Basic_Python/typecasting.py
+++ Basic_Python/typecasting.py
@@ -5,3 +5,7 @@ print(a+b) # the output will be 56 because it is assign as a string
 a = "4"
 b = "5"
 print(int(a)+int(b)) # now it will print 9
+
+c = "1.9"
+d = "5"
+print(float(c) + float(d)) #now it will print 6.9
