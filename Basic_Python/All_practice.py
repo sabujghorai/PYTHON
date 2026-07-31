c75131c9710c17ec195b193fb6e4838b7d235150
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-31 21:20:57 +0530

    updated the all_practice.py file

--- Basic_Python/All_practice.py
+++ Basic_Python/All_practice.py
@@ -5,7 +5,6 @@ a = int(input("Enter the number for multiplication table :"))
 for i in range(1,11):
     print(a,"x", i, "=" ,a*i)
 
-
 # Check if a number is prime.
 
 number = (int(input("Enter the number to check the number is prime or not :")))
