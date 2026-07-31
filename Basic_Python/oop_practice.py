b1c8d3c1f96983d95a18091a2c8283cd032f3366
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-31 21:10:26 +0530

    updated the oop file

--- Basic_Python/oop_practice.py
+++ Basic_Python/oop_practice.py
@@ -1,71 +1,71 @@
-# # Define a Circle class to create a circle  without radius using the constructor
-# # Define Area() method and Perimeter() method 
+# Define a Circle class to create a circle  without radius using the constructor
+# Define Area() method and Perimeter() method 
 
-# class Circle:
-#     def __init__(self,radius):
-#         self.radius = radius
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
 
-#     def Area(self):
-#         return 3.14*self.radius**2
+    def Area(self):
+        return 3.14*self.radius**2
     
-#     def Perimeter(self):
-#         return 2*3.14*self.radius
+    def Perimeter(self):
+        return 2*3.14*self.radius
 
 
-# a = int(input("Enter the radius :"))
-# c2 = Circle(a)
-# print(c2.Area())
-# print(c2.Perimeter())
+a = int(input("Enter the radius :"))
+c2 = Circle(a)
+print(c2.Area())
+print(c2.Perimeter())
 
-# # Define a Employee class with attribute role,department & salary.
-# # the class also have a showDetail() method
-# class Employee:
-#     def __init__(self, role, dpt, salary):
-#         self.role = role
-#         self.dpt = dpt
-#         self.salary = salary
+# Define a Employee class with attribute role,department & salary.
+# the class also have a showDetail() method
+class Employee:
+    def __init__(self, role, dpt, salary):
+        self.role = role
+        self.dpt = dpt
+        self.salary = salary
 
-#     def showDetail(self):
-#         print("Role =", self.role)
-#         print("Department =", self.dpt)
-#         print("Salary =", self.salary)
-
-
-# class Engineer(Employee):
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#         super().__init__("Engineer", "IT", 75000)
-
-# engg1 = Engineer("Elon Musk", 45)
-# engg1.showDetail()
-
-# class Student:
-#     def __init__(self,name,age,branch):
-#         self.name = name
-#         self.age = age
-#         self.branch = branch
-
-#     def display(self):
-#         print(f"Name : {self.name}")
-#         print(f"age : {self.age}")
-#         print(f"Branch : {self.branch}")
+    def showDetail(self):
+        print("Role =", self.role)
+        print("Department =", self.dpt)
+        print("Salary =", self.salary)
 
 
-# Student1 = Student("Sabuj",21,"CSE")
-# Student1.display()
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Engineer", "IT", 75000)
+
+engg1 = Engineer("Elon Musk", 45)
+engg1.showDetail()
+
+class Student:
+    def __init__(self,name,age,branch):
+        self.name = name
+        self.age = age
+        self.branch = branch
+
+    def display(self):
+        print(f"Name : {self.name}")
+        print(f"age : {self.age}")
+        print(f"Branch : {self.branch}")
 
 
-# class Car:
-#     def __init__(self,brand,model,price):
-#         self.brand = brand
-#         self.model = model
-#         self.price = price
+Student1 = Student("Sabuj",21,"CSE")
+Student1.display()
 
-#     def display(self):
-#         print(f"Brand :{self.brand}")
-#         print(f"Model :{self.model}")
-#         print(f"Price :{self.price}")
 
-# car1 = Car("BMW","M4","1.9cr")
-# car1.display()
+class Car:
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def display(self):
+        print(f"Brand :{self.brand}")
+        print(f"Model :{self.model}")
+        print(f"Price :{self.price}")
+
+car1 = Car("BMW","M4","1.9cr")
+car1.display()
