fb7bd334b76cef517e64faa24a567718ae59c5ca
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-04 22:02:57 +0530

    updated the date_time.py file

--- Basic_Python/date_time.py
+++ Basic_Python/date_time.py
@@ -1,35 +1,35 @@
 from datetime import date
-# from datetime import datetime
-# d = date(1926,8,19) # sould be in year,Month and date
-# print(d)
+from datetime import datetime
+d = date(1926,8,19) # sould be in year,Month and date
+print(d)
 
 
-# # Getting today's date
-# today = date.today()
-# print(today)
-# print(today.year) # Prints the present year
-# print(today.month) # prints the present month
-# print(today.day) # prints the present day
+ # Getting today's date
+today = date.today()
+print(today)
+print(today.year) # Prints the present year
+print(today.month) # prints the present month
+print(today.day) # prints the present day
 
 
-# # printing the todays date and time
-# dt = datetime(2006, 9, 6, 16, 50, 40)  # datetime(year, month, day, hour, minute, second)
-# print(dt)
+ # printing the todays date and time
+dt = datetime(2006, 9, 6, 16, 50, 40)  # datetime(year, month, day, hour, minute, second)
+print(dt)
 
 
-# # Printing the today's actual current date and time 
-# now = datetime.now()
-# print(now)
+ # Printing the today's actual current date and time 
+now = datetime.now()
+print(now)
 
-# formatted = now.strftime("%d-%m-%y") # The strftime is changes the format i want
-# day = now.strftime("%d")  # The strftime %d shows the day 
-# month = now.strftime("%m")  # The strftime %m shows the month 
-# year = now.strftime("%y")  # The strftime %y shows the year
+formatted = now.strftime("%d-%m-%y") # The strftime is changes the format i want
+day = now.strftime("%d")  # The strftime %d shows the day 
+month = now.strftime("%m")  # The strftime %m shows the month 
+year = now.strftime("%y")  # The strftime %y shows the year
 
-# print(formatted)
-# print(day)
-# print(month)
-# print(year)
+print(formatted)
+print(day)
+print(month)
+print(year)
 
 
 
@@ -53,15 +53,14 @@ from datetime import date
 
 
 
-# from datetime import datetime
+from datetime import datetime
 
-# now = datetime.now()
+now = datetime.now()
 
-# print(now.strftime("%d/%m/%Y"))
-# print(now.strftime("%H:%M:%S"))
-# print(now.strftime("%A")) # %A for day of that date
-# print(now.strftime("%B")) # %B for month of that date
-
+print(now.strftime("%d/%m/%Y"))
+print(now.strftime("%H:%M:%S"))
+print(now.strftime("%A")) # %A for day of that date
+print(now.strftime("%B")) # %B for month of that date
 
 
 
