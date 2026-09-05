55ae1f8a2836eb00a757f474bf08bfb73c219307
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-06 01:07:04 +0530

    updated the math_module.py file

--- Basic_Python/math_module.py
+++ Basic_Python/math_module.py
@@ -1,4 +1,6 @@
 import math
+
+# WAP to calculate the LCM of two numbers
 A = int(input("Enter first number : "))
 B = int(input("Enter second number : "))
 C = int(input("Enter third number : "))
@@ -7,4 +9,7 @@ E = int(input("Enter fifth number : "))
 F = int(input("Enter sixth number : "))
 
 result = math.lcm(A,B,C,D,E,F)
-print(result)
+print("LCM is :",result)
+
+
+
