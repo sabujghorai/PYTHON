16c21cd1e2028f15ba2d865ceff2e87122e0e6d9
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-06 01:40:19 +0530

    updated the matm module file

--- Basic_Python/math_module.py
+++ Basic_Python/math_module.py
@@ -1,26 +1,26 @@
 import math
 
-# # WAP to calculate the LCM of two numbers
-# A = int(input("Enter first number : "))
-# B = int(input("Enter second number : "))
-# C = int(input("Enter third number : "))
-# D = int(input("Enter foourth number : "))
-# E = int(input("Enter fifth number : "))
-# F = int(input("Enter sixth number : "))
+# WAP to calculate the LCM of two numbers
+A = int(input("Enter first number : "))
+B = int(input("Enter second number : "))
+C = int(input("Enter third number : "))
+D = int(input("Enter foourth number : "))
+E = int(input("Enter fifth number : "))
+F = int(input("Enter sixth number : "))
 
-# result = math.lcm(A,B,C,D,E,F)
-# print("LCM is :",result)
+result = math.lcm(A,B,C,D,E,F)
+print("LCM is :",result)
 
 
 
-# # WAP to calculate the distance between two coordinates 
-# x1 = float(input("X1 :"))
-# x2 = float(input("X2 :"))
-# y1 = float(input("Y1 :"))
-# y2 = float(input("Y2 :"))
+# WAP to calculate the distance between two coordinates 
+x1 = float(input("X1 :"))
+x2 = float(input("X2 :"))
+y1 = float(input("Y1 :"))
+y2 = float(input("Y2 :"))
 
-# Distance = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
-# print(f"Distance is : {Distance}")
+Distance = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+print(f"Distance is : {Distance}")
 
 
 
