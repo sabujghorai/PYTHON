bac1234e4002d428be3fdc204e655c31e33e63ee
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-06 01:04:48 +0530

    updated the math_module.py file

---
+++ Basic_Python/math_module.py
@@ -0,0 +1,10 @@
+import math
+A = int(input("Enter first number : "))
+B = int(input("Enter second number : "))
+C = int(input("Enter third number : "))
+D = int(input("Enter foourth number : "))
+E = int(input("Enter fifth number : "))
+F = int(input("Enter sixth number : "))
+
+result = math.lcm(A,B,C,D,E,F)
+print(result)
